5f2a5331538f1e6e6046957d6416079afc23d969
hemanthchallacs <hemanthchallacs@gmail.com>
2021-10-09 23:56:27 +0530

    mymemcpy

---
+++ C modules/copy.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stddef.h>
+void * copy(const void *, void *, size_t);
+
+int main()
+{
+    char src[] = "hello";
+    printf("size of src %ld\n", sizeof(src));
+    char dest[sizeof(src)];
+    copy(src, dest, sizeof(src));
+    printf("%s\n",dest);
+    return 1;
+}
+
+void * copy(const void *src, void *dest, size_t size){
+
+        char *d = (char *)dest;
+        char *s = (char *)src;
+        for(int i=0; i<size; i++)
+        {
+            *(dest+i) = *(src+i);
+        }
+    return dest;
+}
